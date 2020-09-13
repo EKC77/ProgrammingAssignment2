bf05dbc6530590094b32b47f35a57cb1e2287229
EKC77 <emer_conroy@hotmail.com>
2020-09-13 19:36:23 +0100

    Final

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,47 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function creates an R object that is a list of 4 functions. It also includes two data objects
+## x and im.  The four functions set the value of the matrix, get the value of the vector
+## works out the value of the inverse and get the value of the im
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  im <- NULL   
+  
+  set <- function(y) {     
+    x <<- y
+    im <<- NULL
+    return(y)
+  }
+  
+  get <- function() x      
+  setim <- function(solve) im <<- solve    
+  getim <- function() im                
+  
+  list(set = set, get = get,   
+       setim = setim,
+       getim = getim)
 }
-
 
 ## Write a short comment describing this function
+## if im (inverse matrix) is NOT null it will print message 'getting chached data' and print the cached im.
+## This only happens if you have run already. If it is null it has not been calcualtated yet 
+## and the solve functoin is carried out on your matrix and a new im is returned.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {    
+  im <- x$getim()  
+  if(!is.null(im)) {
+    message("getting cached data")
+    return(im)
+  }
+  data <- x$get()  
+  im <- solve(data, ...)  
+  x$setim(im)    
+  im
 }
+
+## TO TEST
+m1 <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+myMatrix <- makeCacheMatrix(m1)
+cacheSolve(myMatrix)  
